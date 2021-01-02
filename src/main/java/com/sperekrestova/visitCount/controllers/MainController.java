90a68147915f146bd7684488f350af152d6cd27b
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2021-01-02 21:28:50 +0300

    Added role-based auth

--- src/main/java/com/sperekrestova/visitCount/controllers/MainController.java
+++ src/main/java/com/sperekrestova/visitCount/controllers/MainController.java
@@ -1,14 +1,8 @@
 package com.sperekrestova.visitCount.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Svetlana
@@ -32,12 +26,8 @@ public class MainController {
         return "marks";
     }
 
-    @GetMapping("/logout")
-    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/";
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        return "admin";
     }
 }
