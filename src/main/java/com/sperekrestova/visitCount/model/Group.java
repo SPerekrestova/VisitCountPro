ec0470114a0799e8354993a49b40c27075b5e581
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2021-01-02 21:34:26 +0300

    Added models skeletons

--- src/main/java/com/sperekrestova/visitCount/model/Group.java
+++ src/main/java/com/sperekrestova/visitCount/model/Group.java
@@ -1,8 +1,14 @@
 package com.sperekrestova.visitCount.model;
 
+import java.util.List;
+
 /**
  * Created by Svetlana
  * Date: 02.01.2021
  */
 public class Group {
+
+    private String groupName;
+    private List<Student> students;
+
 }
---
+++ src/main/java/com/sperekrestova/visitCount/model/Student.java
@@ -0,0 +1,12 @@
+package com.sperekrestova.visitCount.model;
+
+/**
+ * Created by Svetlana
+ * Date: 02.01.2021
+ */
+public class Student {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Group group;
+}
