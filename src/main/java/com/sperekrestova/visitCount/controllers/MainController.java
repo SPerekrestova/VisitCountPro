dca41f55fd627834fc3c350dc037daedca1689df
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2021-01-02 20:48:37 +0300

    Added logout button

--- src/main/java/com/sperekrestova/visitCount/controllers/MainController.java
+++ src/main/java/com/sperekrestova/visitCount/controllers/MainController.java
@@ -1,8 +1,14 @@
 package com.sperekrestova.visitCount.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Svetlana
@@ -24,5 +30,14 @@ public class MainController {
     @GetMapping("/marks")
     public String marks(Model model) {
         return "marks";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
