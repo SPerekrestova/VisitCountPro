f0c78644fac6ae13300ff2d1ba13a2ee1010750e
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2021-01-10 19:17:39 +0300

    Added custom login page

--- src/main/java/com/sperekrestova/visitCount/WebSecurityConfig.java
+++ src/main/java/com/sperekrestova/visitCount/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -46,14 +47,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                    .antMatchers("/register").permitAll()
                     .antMatchers("/**").authenticated()
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .anyRequest().permitAll()
-                .and()
-                    .formLogin()
-                    .usernameParameter("email")
-                    .defaultSuccessUrl("/").permitAll()
-                .and()
-                    .exceptionHandling().accessDeniedPage("/403");
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+        http.csrf().disable();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
 }
--- src/main/java/com/sperekrestova/visitCount/controllers/MainController.java
+++ src/main/java/com/sperekrestova/visitCount/controllers/MainController.java
@@ -43,4 +43,15 @@ public class MainController {
         model.addAttribute("lectureGroups", user.getLectureGroups());
         return "groups";
     }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("errorMsg", "Your username and password are invalid.");
+
+        if (logout != null)
+            model.addAttribute("msg", "You have been logged out successfully.");
+
+        return "login";
+    }
 }
