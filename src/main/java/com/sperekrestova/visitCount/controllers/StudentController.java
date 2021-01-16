8e97a5a5ccbb2d958e53a005cc08a8d9c0c4aec2
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2021-01-16 17:42:10 +0300

    Added student's personal card

---
+++ src/main/java/com/sperekrestova/visitCount/controllers/StudentController.java
@@ -0,0 +1,28 @@
+package com.sperekrestova.visitCount.controllers;
+
+import com.sperekrestova.visitCount.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Svetlana
+ * Date: 16.01.2021
+ */
+@Controller
+@AllArgsConstructor
+@RequestMapping("/student")
+public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    @GetMapping("/{id}")
+    public String showStudent(@PathVariable("id") int id, Model model) {
+        model.addAttribute("student", studentRepository.findById((long) id).get());
+        return "student";
+    }
+
+}
